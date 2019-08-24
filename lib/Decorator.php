7e088f55aef1ee6e44ed27a7a76e119523e7e81a
loyalpond60 <mirrormepan@gmail.com>
2019-08-25 00:22:28 +0800

    Add files via upload

--- lib/Decorator.php
+++ lib/Decorator.php
@@ -23,19 +23,47 @@ class HeadDecorator extends Widget
     protected function html_header(){
 
         $msg=<<<HTML
+        <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+	<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+	<head>
+	<title>Ftm designs</title>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<meta name="author" content="fatma.hassan@gmail.com" />
+	<meta name="description" content="FTM" />
+
+
+	<link rel="stylesheet" href="lib/touching.css" type="text/css" />
+	</head>
+			<body>
+				<div id="container">
+				<div id="banner">
+				<div id='bannertitle'>Touching</div>
+				</div>
+
+				<div id="outer">
+ 				<div id="inner">
+ 				<div id="left">
+  <div class="verticalmenu">
+ </div> 
+ </div>
+	<div id="content">
 
 HTML;
         return $msg;
     }
     protected function bar(){
         $msg=<<<HTML
-
 HTML;
         return $msg;
     }
     protected function html_footer(){
         $msg=<<<HTML
-
+        			</div>
+				</div>
+				</div>
+ 				<div id="footer"><h1><a href="http://freethoughts.ftmblog.com">copyrights&copy;FTM</a></h1></div>
+				</div>
+	</body></html>
 HTML;
         return $msg;
     }
